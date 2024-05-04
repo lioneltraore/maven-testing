53757635df7b6848521054a9436fc62c5e503e84
Lionel Traore <hello@lioneltraore.com>
2024-05-04 14:21:04 +0000

    naming tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,4 +3,8 @@ public class Calculator {
     public int integerDivision(int dividend, int divisor) {
         return dividend / divisor;
     }
+
+    public int integerSubstraction(int minuend, int substrahend) {
+        return minuend - substrahend;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,13 +1,31 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test math operations in Calculator class")
 class CalculatorTest {
 
+    // test<System under test>_<Condition or state change>_<Expected result>
     @Test
-    void integerDivision() {
+    @DisplayName("Test 6/2 = 3")
+    void testIntegerDivision_whenSixIsDividedByTwo_shouldReturnThree() {
         Calculator calculator = new Calculator();
         int result = calculator.integerDivision(6, 2);
-        assertEquals(3, result);
+        assertEquals(3, result, "6/2 did not produce 3");
+    }
+
+    @Test
+    @DisplayName("Division by zero")
+    void testIntegerDivision_whenDividendIsDividedByZero_shouldThrowArithmeticException() {
+        fail("Not implemented");
+    }
+
+    @Test
+    @DisplayName("Test 15-5 = 10")
+    void integerSubstraction() {
+        Calculator calculator = new Calculator();
+        int result = calculator.integerSubstraction(15, 5);
+        assertEquals(10, result);
     }
 }
